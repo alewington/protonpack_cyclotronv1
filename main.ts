76c50b5d37e311c560e394fd6b50840cc7749c39
alewington <10705089+alewington@users.noreply.github.com>
2020-10-24 15:54:37 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+function cyclotron () {
+    while (true) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        basic.pause(750)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        basic.pause(750)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        basic.pause(750)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P3, 1)
+        basic.pause(750)
+    }
+}
+basic.forever(function () {
+    cyclotron()
+})
